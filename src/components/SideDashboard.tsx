27c5d4a736f443b15d756cc0844a4cdbd1183f88
atulc036 <lcb2022036@iiitl.ac.in>
2023-10-02 21:36:05 +0530

    Changes made to homepage.

--- src/components/SideDashboard.tsx
+++ src/components/SideDashboard.tsx
@@ -3,28 +3,11 @@ import React from 'react'
 
 const SideDashboard = () => {
   return (
-<<<<<<< HEAD
-    <div className=" bg-slate-900 mt-0 w-1/4 text-white">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio natus
-    numquam, commodi sint animi laudantium qui rerum exercitationem et aliquam
-    eligendi modi minima laboriosam quis ratione architecto veritatis
-    molestias placeat eos odio voluptas? Nam sit distinctio doloribus. Aperiam
-    necessitatibus blanditiis dolores maiores. Dolorem voluptatum libero,
-    ullam voluptas cumque corporis totam! At nemo exercitationem, quam error
-    asperiores, vel ratione perferendis laboriosam dolore animi laborum, eum
-    beatae est quis tempora. Modi reiciendis saepe cupiditate maiores quis
-    nisi possimus debitis delectus repellat laboriosam doloremque similique
-    eius harum quaerat obcaecati architecto, sed enim asperiores. Tempore quas
-    neque, saepe suscipit fugit maiores repellat, facere id culpa at nulla ut!
-    Provident aspernatur tempore accusantium ea rerum atque, perferendis totam
-    explicabo laboriosam eligendi repudiandae repellendus aliquam est culpa
 
-    </div>
-=======
   <VStack className='text-white h-full' gap={2} pt={4}>
   <Heading fontSize={"2xl"} className='font'>TradeIn</Heading>
   </VStack>
->>>>>>> 5705e7a3f2f6d00d3d9ad657ebc5ed45a94fd829
+
   )
 }
 
