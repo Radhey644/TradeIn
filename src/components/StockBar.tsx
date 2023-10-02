7180006f002c7540383fb420367084810f9d9b14
pankaj kumar <pankajsingh92533@gmail.com>
2023-10-02 22:42:16 +0530

    placement of button corrected..

--- src/components/StockBar.tsx
+++ src/components/StockBar.tsx
@@ -5,9 +5,9 @@ import React, { useState } from "react";
 const StockBar = () => {
   const [watchlist, setWatchlist] = useState(false);
   return (
-    <div className="flex-row p-3 text-white gap-4 flex-1">
+    <div className="flex-row  p-3 text-white gap-4 flex-1 ">
       <VStack className="float-left">
-        <Heading color={"white"} fontSize={"xl"} className="-left-12 relative">
+        <Heading color={"white"} fontSize={"xl"} className="-left-12 ">
           BBCA
         </Heading>
         <span className="text-white opacity-70 text-xs">
@@ -24,22 +24,24 @@ const StockBar = () => {
       </VStack>
 
       {/* this will be a group of buttons to sell and buy stocks */}
-      <div className="flex-row gap-2 flex-1 items-center">
-        <button
-          className={`${watchlist?"bg-white text-black border-black":"bg-transparent"} divide-solid border px-1 py-1 rounded-md text-xs active:scale-95`}
-          onClick={() => {
-            setWatchlist(!watchlist);
-          }}
-        >
-          {watchlist?"Added to Watchlist":"Add to Watchlist"}
-        </button>
-        <button className="bg-red-400 px-5 py-1 rounded-md text-black font-bold text-sm">
-          Sell
-        </button>
-        <button className="bg-green-300 px-5 py-1 rounded-md text-black font-bold text-sm">
-          Buy
-        </button>
-      </div>
+      <div className="flex flex-row-reverse gap-2 flex-1 p-2 items-center space-x-2">
+  
+  <button
+    className={`${watchlist?"bg-white text-black border-black":"bg-transparent"} divide-solid border px-2 py-1 rounded-md text-xs active:scale-95`}
+    onClick={() => {
+      setWatchlist(!watchlist);
+    }}
+  >
+    {watchlist?"Added to Watchlist":"Add to Watchlist"}
+  </button>
+  <button className="bg-red-400 px-5 py-1 rounded-md text-black font-bold text-sm">
+    Sell
+  </button>
+  <button className="bg-green-300 px-5 py-1 rounded-md text-black font-bold text-sm">
+    Buy
+  </button>
+</div>
+
     </div>
   );
 };
