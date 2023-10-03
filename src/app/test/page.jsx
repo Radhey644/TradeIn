92ccc7be822cdbe446657f3fa29dcf1c377cdc6a
atulc036 <lcb2022036@iiitl.ac.in>
2023-10-04 00:48:12 +0530

    Added a stock search feature.

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,44 +1,65 @@
-"use client";
+"use client"
 import Home from "@/components/Home";
 import { Input, Spinner } from "@chakra-ui/react";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
+
+// Define the debounce function
+function debounce(func, delay) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
 
 const Test = () => {
   const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
-  useEffect(() => {
-    fetch("https://twelve-data1.p.rapidapi.com/symbol_search", {
+
+  // Create a debounced version of the function that makes API requests
+  const debouncedFetchData = debounce((searchQuery) => {
+    fetch(`https://twelve-data1.p.rapidapi.com/symbol_search?symbol=${searchQuery}`, {
       params: {
-        symbol: query,
         outputsize: "4",
       },
       method: "GET",
-      // You should specify the HTTP method
       headers: {
         "X-RapidAPI-Key": "cc975687aemshc0763b961baa52ep1836fcjsn7d06c55309e4",
       },
     })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setData(data.data);
+      .then((res) => res.json())
+      .then((responseData) => {
+        setData(responseData.data);
       })
       .catch((err) => {
-        console.log("error occured");
+        console.log("Error occurred:", err);
       });
-  }, []);
-  // if (!data)
-  //   return (
-  //     <Spinner
-  //       thickness="4px"
-  //       speed="0.65s"
-  //       emptyColor="gray.200"
-  //       color="blue.500"
-  //       size="xl"
-  //     />
-  //   );
+  }, 2000); // Adjust the debounce delay (1 second in this case)
+
+  // Effect to watch for changes in the 'query' state
+  useEffect(() => {
+    if(query.length>2)
+    {
+       debouncedFetchData(query);
+    }
+   
+  }, [query]);
+
+  if(!data)
+  {
+    return <Spinner
+    thickness='4px'
+    speed='0.65s'
+    emptyColor='gray.200'
+    color='blue.500'
+    size='xl'
+  />
+  }
+
   return (
     <>
       <Input
@@ -51,7 +72,8 @@ const Test = () => {
       />
       {data &&
         data.map((item) => {
-          return <li>{item.instrument_name}</li>;
+          return <li className="cursor-pointer list-none" >
+            <Link href={`/test/${item.symbol}`}>{item.instrument_name}</Link></li>;
         })}
     </>
   );
