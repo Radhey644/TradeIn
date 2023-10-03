d6a60c0c6f5734017a7c249c0fa68b68790c8044
pankaj kumar <pankajsingh92533@gmail.com>
2023-10-04 00:44:23 +0530

    worked on Authentication ,login ,signup,api

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,40 @@
+"use client"
 import { Divider, Spacer } from "@chakra-ui/react";
 import React from "react";
+import axios from "axios";
+import {toast} from "react-toastify";
+
+import router from "next/router";
 
 const Navbar = () => {
+
+
+  const logout = async () => {
+    try {
+        await axios.get('/api/users/logout')
+        toast.success('Logout successful')
+        location.reload()
+        router.push('/login')
+        
+    } catch (error:any) {
+        console.log(error.message);
+        toast.error(error.message)
+    }
+}
+
   return (
     <div className="overflow-x-hidden ">
-        {/* <Divider
+      {/* <Divider
         className="inline-block"
           orientation="vertical" // Set the orientation to vertical
            // Adjust the margin as needed
           h="76px" // Adjust the height as needed
         /> */}
       <div className="flex flex-col sm:flex-row bg-slate-900 p-2 gap-4 text-white">
-        
         {/* <div className="flex-shrink-0 font-bold text-xl text-center sm:text-left self-center">
           TradeIn
         </div> */}
-          <div className="order-2 flex flex-col sm:flex-row p-1 h-1/2 justify-center items-center self-center border border-gray-400 rounded-md ml-1">
+        <div className="order-2 flex flex-col sm:flex-row p-1 h-1/2 justify-center items-center self-center border border-gray-400 rounded-md ml-1">
           <input
             type="text"
             className="search-input bg-transparent text-white p-1  outline-none w-full sm:w-64 md:w-96 text-xs" // Adjusted padding and font size
@@ -25,10 +44,9 @@ const Navbar = () => {
             Search
           </button> */}
         </div>
-        
-        <Spacer/>
+
+        <Spacer />
         {/* Spacer for small screens */}
-        
 
         <div className="order-3 flex m-2 p-2 gap-4">
           <i
@@ -44,6 +62,10 @@ const Navbar = () => {
             src="https://i.pinimg.com/originals/1e/c4/62/1ec4624b6606c8c358e41c85ccdd20a9.jpg"
             alt=""
           />
+          {/* Logout button */}
+          <button onClick={logout} className="bg-green-500 hover:bg-green-700 text-white font-bold px-1.5 rounded-full">
+            Logout
+          </button>
           {/* <div className="flex flex-col relative -top-0.4 ">
             <span className="font-medium text-sm">Pankaj Kumar</span>
             <span className="font-light text-xs">lcb2022039@gmail.com</span>
@@ -51,7 +73,6 @@ const Navbar = () => {
         </div>
 
         {/* Search bar */}
-       
       </div>
     </div>
   );
