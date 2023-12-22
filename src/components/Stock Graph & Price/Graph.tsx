747ca615ef5031aacda948fa5896a68d8fa228cc
atulc036 <lcb2022036@iiitl.ac.in>
2023-12-22 17:50:43 +0530

    Update package.json, lock files, and add a stock graph component

--- src/components/Stock Graph & Price/Graph.tsx
+++ src/components/Stock Graph & Price/Graph.tsx
@@ -12,39 +12,9 @@ import {
   AreaChart,
   Area,
 } from "recharts";
-const data = [
-  { Date: '10/16/2023', Price: 0.013135531834577802 },
-  { Date: '10/17/2023', Price: 0.013135531834577802 },
-  { Date: '10/18/2023', Price: 0.011913582049249584 },
-  { Date: '10/19/2023', Price: 0.009739495713289043 },
-  { Date: '10/20/2023', Price: 0.009706701116487802 },
-  { Date: '10/21/2023', Price: 0.00568820932405429 },
-  { Date: '10/22/2023', Price: 0.004975849255372646 },
-  { Date: '10/23/2023', Price: 0.005175243187754998 },
-  { Date: '10/24/2023', Price: 0.005097735311922618 },
-  { Date: '10/25/2023', Price: 0.005201108519294425 },
-  { Date: '10/26/2023', Price: 0.005309335560474713 },
-  { Date: '10/27/2023', Price: 0.005182592895588143 },
-  { Date: '10/28/2023', Price: 0.004865836907470763 },
-  { Date: '10/29/2023', Price: 0.0048459540056011614 },
-  { Date: '10/30/2023', Price: 0.00493515891937446 },
-  { Date: '10/31/2023', Price: 0.00501763021661517 },
-  { Date: '11/1/2023', Price: 0.0048973833088963616 },
-  { Date: '11/2/2023', Price: 0.004941993539977771 },
-  { Date: '11/3/2023', Price: 0.004931221168753471 },
-  { Date: '11/4/2023', Price: 0.0050336619109299045 },
-  { Date: '11/5/2023', Price: 0.005252819092754764 },
-  { Date: '11/6/2023', Price: 0.0053318816555861645 },
-  { Date: '11/7/2023', Price: 0.005320740404588513 },
-  { Date: '11/8/2023', Price: 0.0051613756809782575 },
-  { Date: '11/9/2023', Price: 0.005239721794845901 },
-  { Date: '11/10/2023', Price: 0.006150711097996899 },
-  { Date: '11/10/2023', Price: 0.006364204044577569 }
-]
-
-const Graph = () => {
+const Graph = ({data}:Array<Object>) => {
   return (
-    <div className="flex-1 w-4/5 m-2">
+    <>
       {/* <LineChart
         width={500}
         height={350}
@@ -63,7 +33,7 @@ const Graph = () => {
       </LineChart> */}
       <AreaChart
       width={800}
-      height={400}
+      height={500}
       data={data}
       margin={{
         top: 10,
@@ -72,13 +42,18 @@ const Graph = () => {
         bottom: 0
       }}
     >
-      <CartesianGrid strokeDasharray="3 3" />
+      <defs>
+    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#33adff" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#33adff" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
       <XAxis dataKey="Date" />
       <YAxis />
       <Tooltip />
-      <Area type="monotone" dataKey="Price" stroke="#53c68c" strokeWidth={0.2} fill="#53c68c" fillOpacity={0.3} />
+      <Area type="monotone" dataKey="Price"  stroke="#33adff" strokeWidth={3} fill="url(#colorUv)" fillOpacity={0.3} />
     </AreaChart>
-    </div>
+    </>
   );
 };
 
--- src/components/UI-asset/Home.tsx
+++ src/components/UI-asset/Home.tsx
@@ -3,9 +3,26 @@ import SideDashboard from "../SideDashboard/SideDashboard";
 import StockBar from "./StockBar";
 import Graph from "../Stock Graph & Price/Graph";
 import Price from "../Stock Graph & Price/Price";
-import TradeInNavbar from "../Navbar/Navbar";
 
 const Home = () => {
+  const data = [
+    { Date: "10/16/2023", Price: 0.013135531834577802 },
+    { Date: "10/17/2023", Price: 0.013135531834577802 },
+    { Date: "10/18/2023", Price: 0.011913582049249584 },
+    { Date: "10/19/2023", Price: 0.009739495713289043 },
+    { Date: "10/20/2023", Price: 0.009706701116487802 },
+    { Date: "10/21/2023", Price: 0.00568820932405429 },
+    { Date: "10/22/2023", Price: 0.004975849255372646 },
+    { Date: "10/23/2023", Price: 0.005175243187754998 },
+    { Date: "10/24/2023", Price: 0.005097735311922618 },
+    { Date: "10/25/2023", Price: 0.005201108519294425 },
+    { Date: "10/26/2023", Price: 0.005309335560474713 },
+    { Date: "10/27/2023", Price: 0.005182592895588143 },
+    { Date: "10/28/2023", Price: 0.004865836907470763 },
+    { Date: "10/29/2023", Price: 0.0048459540056011614 },
+    { Date: "10/30/2023", Price: 0.00493515891937446 },
+    { Date: "10/31/2023", Price: 0.00501763021661517 },
+  ];
   return (
     // this is the main container having both sides
     <div className="flex flex-row bg-black min-h-screen">
@@ -21,7 +38,7 @@ const Home = () => {
         {/* this part is for graph and the prices */}
         <div className="flex gap-2 xs:flex-col flex-wrap">
           {/* as the graph and the prices are in same line  */}
-          <Graph />
+          <Graph data={data} />
           <Graph />
           <Graph />
           <Graph />
